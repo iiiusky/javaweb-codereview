1518582002ff672eee7cab15003decdb79bf60d0
yz <admin@javaweb.org>
2019-01-08 14:45:12 +0800

    修改调用链输出

--- javaweb-codereview-agent/src/main/java/org/javaweb/codereview/utils/ClassUtils.java
+++ javaweb-codereview-agent/src/main/java/org/javaweb/codereview/utils/ClassUtils.java
@@ -5,13 +5,17 @@ package org.javaweb.codereview.utils;
  */
 public class ClassUtils {
 
+	/**
+	 * 打印调用链信息
+	 */
 	public static void printStackTrace() {
 		StackTraceElement[] elements = Thread.currentThread().getStackTrace();
 
 		for (StackTraceElement element : elements) {
 			System.err.println(element);
-			System.err.println("--------------------------------------------------------------------------");
 		}
+
+		System.err.println("--------------------------------------------------------------------------");
 	}
 
 }
